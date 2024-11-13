57728c45817f769e5604be1466f5ea40a0854374
TAMMIREDDYSIDDU0 <93256084+TAMMIREDDYSIDDU0@users.noreply.github.com>
2024-11-13 12:04:33 +0530

    Changes for deployment

--- src/main/java/com/arista/nestnavigator/NestNavigatorApplication.java
+++ src/main/java/com/arista/nestnavigator/NestNavigatorApplication.java
@@ -10,15 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NestNavigatorApplication implements CommandLineRunner {
-    @Autowired
-    AuthenticationService authenticationService;
+//    @Autowired
+//    AuthenticationService authenticationService;
 
     public static void main(String[] args){
         SpringApplication.run(NestNavigatorApplication.class, args);
     }
     @Override
     public void run(String ...args){
-    User user = new User("Siddu","Tammireddy","abc@gmail.com","9876543210","sidda_sai","password");
-    authenticationService.register(user);
+//    User user = new User("Siddu","Tammireddy","abc@gmail.com","9876543210","sidda_sai","password");
+//    authenticationService.register(user);
     }
 }
