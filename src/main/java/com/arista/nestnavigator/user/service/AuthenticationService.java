c2a92cd0ddb689c0bf4903d556c2a1218589f22e
KenaxisQ <kenaxisQ@gmail.com>
2024-11-13 10:33:51 +0530

    Update AuthenticationService.java

--- src/main/java/com/arista/nestnavigator/user/service/AuthenticationService.java
+++ src/main/java/com/arista/nestnavigator/user/service/AuthenticationService.java
@@ -16,6 +16,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
