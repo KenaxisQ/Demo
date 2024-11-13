c843bdd9f83c83c17b75689d5e05939409e7364a
TAMMIREDDYSIDDU0 <93256084+TAMMIREDDYSIDDU0@users.noreply.github.com>
2024-11-13 12:49:02 +0530

    Changes for deployment generating war files

--- src/main/java/com/arista/nestnavigator/NestNavigatorApplication.java
+++ src/main/java/com/arista/nestnavigator/NestNavigatorApplication.java
@@ -7,9 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NestNavigatorApplication implements CommandLineRunner {
+public class NestNavigatorApplication extends SpringBootServletInitializer implements CommandLineRunner {
 
 //    private final AuthenticationService authenticationService;
 //    public NestNavigatorApplication(AuthenticationService authenticationService) {
